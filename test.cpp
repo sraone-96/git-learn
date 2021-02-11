623283363b0d9cb8851e44cc7ed242df17e8c642
sravan <saisravan.mylavarapu@gmail.com>
2021-02-11 17:00:48 +0530

    testing again for changes in branch

--- test.cpp
+++ test.cpp
@@ -1,6 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-// just for testing the actual PR issue
 class wallet
 {
 private:
@@ -78,9 +77,6 @@ void transfer(user& ac2, double amt)
 		{
 			credit(*this,10);
 			ac2.credit(ac2,10);
-			// for no transaction, could use below:
-			// this.bal+=10;
-			// ac2.bal+=10;
 		}
 
 }
